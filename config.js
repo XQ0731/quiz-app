4415bbed50cde7217a1ac9a50e862a50b3cf9b28
XQ0731 <Zxy2668661390@163.com>
2025-11-04 01:39:07 +0800

    config.js
    Updated GITHUB_TOKEN with a new value.

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    GITHUB_TOKEN: '这里粘贴您的真实Classic Token',
+    GITHUB_TOKEN: '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',
     REPO_OWNER: 'XQ0731',
     REPO_NAME: 'quiz-app',
     LABEL: 'achievement'  // 改为英文标签，避免编码问题
