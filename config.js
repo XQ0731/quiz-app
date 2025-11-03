d8f76c10d1597145182f832b73a757c1ddb28991
XQ0731 <Zxy2668661390@163.com>
2025-11-04 01:35:14 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    GITHUB_TOKEN: '这里粘贴您的真实令牌',
+    GITHUB_TOKEN: '这里粘贴您的真实Classic Token',
     REPO_OWNER: 'XQ0731',
-    REPO_NAME: 'quiz-app', 
-    LABEL: '成绩记录'
+    REPO_NAME: 'quiz-app',
+    LABEL: 'achievement'  // 改为英文标签，避免编码问题
 };
